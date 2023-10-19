978c0abbcabb3b7a65bd1f2ab3e0ace7c3cd6d50
Saleh Al-Saleh <saleh.alsaleh802@gmail.com>
2023-10-19 17:41:21 +0300

    add gameBoard class & implement func winnerChecks

--- script.js
+++ script.js
@@ -10,4 +10,61 @@ return {
 
 
 
+const gameboard = (function (){
+    
+    let cells = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
 
+    let checkRows = (sign) => {
+        for(let i = 0; i < cells.length; i++ ){
+
+            if(cells[i].every(cell => cell == sign)){
+                return isWinner;
+            }
+        }
+        return false;
+    }
+
+    checkColumns = (sing) => {
+        let column = []
+        for(let i = 0; i<cells.length; i++){
+            for(let j=0; j<cells[i].length; j++){
+                column.push( cells[j][i]);
+            }
+
+            if(column.every(cell => cell == sign)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    checkDiagnals = (sign) => {
+        let diagonal = [];
+        //diagonal
+       for (let i = 0; i < cells.length; i++) {
+        diagonal.push(cells[i][i]); 
+        
+       }
+       if(diagonal.every(cell => cell == sign)){
+        return true;
+       }
+
+       let revDiagonal = [];
+       //reverse diagonal
+       for (let i = 0; i < cells.length; i++) {
+        revDiagonal = cells[i][cells.length - i-1]
+       }
+       if(revDiagonal.every(cell => cell == sign )){
+        return true;
+       }
+
+       return false;
+
+    }
+
+return {}
+})();
