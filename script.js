07a08033ebec4aad1e05ba5fad063dce365ac6d1
Saleh Al-Saleh <saleh.alsaleh802@gmail.com>
2023-10-24 20:23:32 +0300

    add all features and finish project

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ return {
 }
 
 
-
 const gameboard = (function (){
     
     let cells = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
     ];
 
     let getCells = () => {
@@ -42,16 +41,16 @@ const gameboard = (function (){
     }
 
     let resetCells = () => {
-        getCells = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ];
+        cells = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+            ];
     }
 
     let checkColumns = (sign) => {
-        let column = []
         for(let i = 0; i<cells.length; i++){
+        let column = []
             for(let j=0; j<cells[i].length; j++){
                 column.push( cells[j][i]);
             }
@@ -89,13 +88,109 @@ const gameboard = (function (){
     }
 
     let CheckWinner = (sign) => {
-        
+        debugger;
         if(checkRows(sign) || checkColumns(sign) || checkDiagnals(sign) ){
+        
             return true;
         }
+        return false;
+    }
+
+    //check if there is more steps to play or ended with draw
+    let checkGameStatus = () =>
+    {
+        for (let i = 0; i < cells.length; i++) {
+            const row = cells[i];
+            if(row.includes(0))
+            //there more moves to play
+            return true;
+            
+        }
+        //no moves => game result : draw
+        return false;
     }
 
     
 
-return {CheckWinner, getCells, resetCells, chooseMove}
+return {CheckWinner, getCells, resetCells, chooseMove, checkGameStatus}
 })();
+
+const displayAndButtonController = (()=>{
+    
+    const xtxt = document.querySelector('#x');
+    const otxt = document.querySelector('#o');
+
+    const restButton = document.querySelector('#resetBtn');
+
+    const cellCont = document.querySelector('.gameCont');
+    const cellArr = cellCont.children;
+
+    let glow = (playerTurn) => {
+        const glowClass = "glowPlayerBg";
+        if( playerTurn == 'o'){
+            otxt.classList += " " + glowClass;
+            xtxt.classList.remove(glowClass);
+         }else{
+            xtxt.classList += " " + glowClass;
+            otxt.classList.remove(glowClass);
+
+         }
+    }
+    
+    
+    return {otxt, xtxt, restButton, cellArr, glow};
+})();
+
+const gameController = (()=>{
+
+
+    const player1 = player('x');
+    const player2 = player('o');
+
+    let playerTurn = player1;
+    let gameStat = true;
+
+    displayAndButtonController.glow(playerTurn.getSign);
+
+    for(let i = 0; i<gameboard.getCells().length; i++){
+    for(let j = 0; j<gameboard.getCells()[i].length; j++){
+        displayAndButtonController.cellArr[(3*i)+j]
+        .addEventListener('click' , () =>{
+            if(gameboard.chooseMove(i,j,playerTurn.getSign())){
+
+                displayAndButtonController.cellArr[(3*i)+j]
+                .firstChild.textContent = playerTurn.getSign(); 
+                console.log(gameboard.getCells());
+
+                if(gameboard.CheckWinner(playerTurn.getSign())){
+                    alert('the winner is ' + playerTurn.getSign());   
+                }
+
+                if(gameboard.checkGameStatus()){
+                    playerTurn = playerTurn == player1 ? player2 : player1;
+                    displayAndButtonController.glow(playerTurn.getSign());
+                }else{
+                    alert('draw');
+                }
+
+            }else{
+                alert('already taken');
+            }
+        });
+
+        displayAndButtonController.restButton.addEventListener('click', 
+        () => {
+            gameboard.resetCells();
+            playerTurn = player1;
+            displayAndButtonController.glow(playerTurn.getSign())
+            for(let i = 0; i<displayAndButtonController.cellArr.length; i++)
+            displayAndButtonController.cellArr[i].firstChild.textContent = "";
+        });
+        
+    }
+}
+
+
+
+})();
+
